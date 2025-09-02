ddbf743b00cb7a7f5c304a19379c5b43ccfa8c83
Ivan Campos <sopmac21379@gmail.com>
2025-09-02 11:08:11 -0400

    Initial Commit

---
+++ Facial Computing/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Facial Computing
+//
+//  Created by IVAN CAMPOS on 9/2/25.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            ToggleImmersiveSpaceButton()
+        }
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
---
+++ Facial Computing/Facial_ComputingApp.swift
@@ -0,0 +1,40 @@
+//
+//  Facial_ComputingApp.swift
+//  Facial Computing
+//
+//  Created by IVAN CAMPOS on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Facial_ComputingApp: App {
+    
+    @State private var appModel = AppModel()
+    @State private var avPlayerViewModel = AVPlayerViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            if avPlayerViewModel.isPlaying {
+                AVPlayerView(viewModel: avPlayerViewModel)
+            } else {
+                ContentView()
+                    .environment(appModel)
+            }
+        }
+        
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                    avPlayerViewModel.play()
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                    avPlayerViewModel.reset()
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
